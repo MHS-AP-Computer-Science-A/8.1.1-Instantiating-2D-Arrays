25faa45153e7ca4144a63e133604cd922bd10dec
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-01-26 12:29:44 -0500

    Add files via upload

---
+++ src/Instantiate.java
@@ -0,0 +1,36 @@
+
+public class Instantiate {
+
+	public static void main(String[] args) {
+		// 1. Create myArray here using an initializer list
+
+		
+		
+		
+		// Uncomment below when you have created the array
+		//print(myArray);
+		
+		
+		// 2. Create myArray2 here using row and column numbers
+
+
+	
+		// Uncomment below when you have created the array
+		//print(myArray2);
+
+	}
+	
+    //Do not make alterations to this method!
+    public static void print(int[][] array)
+    {
+        for(int[] row: array)
+        {
+            for(int num: row)
+            {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
